41b03e0dfe00b80818fddb1416b894b854c0874b
Artem Shcherban <artshcherban@gmail.com>
2022-04-07 08:01:57 +0200

    Initial commit

---
+++ Collections/SceneDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SceneDelegate.swift
+//  Collections
+//
+//  Created by Artem Shcherban on 07.04.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+}
---
+++ Collections/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Collections
+//
+//  Created by Artem Shcherban on 07.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
