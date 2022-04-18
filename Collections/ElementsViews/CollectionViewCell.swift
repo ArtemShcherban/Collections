423d973bb05dee3193ab15cb98029d4e27317686
Artem Shcherban <artshcherban@gmail.com>
2022-04-18 07:06:03 +0200

    Merge CollectionViewCell.swift from task_1.6_Dictionary

--- Collections/ElementsViews/CollectionViewCell.swift
+++ Collections/ElementsViews/CollectionViewCell.swift
@@ -16,15 +16,23 @@ class CollectionViewCell: UICollectionViewCell {
         tempTitleTextLabel.numberOfLines = 0
         tempTitleTextLabel.textColor = ColorsConstants.blueColor
         tempTitleTextLabel.sizeToFit()
+        tempTitleTextLabel.backgroundColor = .green
         return tempTitleTextLabel
     }()
     
-    func cellConfigure() {
+    func cellConfigure(_ cellTitle: String) {
+        layer.borderWidth = 0.2
+        layer.borderColor = UIColor.black.cgColor
+        titleTextLabel.text = cellTitle
         self.addSubview(titleTextLabel)
         setTitleTextLabelConstraints()
     }
     
     func setTitleTextLabelConstraints() {
-        
+        titleTextLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([titleTextLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+                                     titleTextLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+                                     titleTextLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: self.frame.height / 5),
+                                     titleTextLabel.bottomAnchor.constraint(lessThanOrEqualTo: self.bottomAnchor)])
     }
 }
