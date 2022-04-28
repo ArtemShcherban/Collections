b11c3b287e4d0907e155aefdd496efec506c7c83
Artem Shcherban <artshcherban@gmail.com>
2022-04-28 10:44:02 +0200

    Commit after added new SetModuleUnitTest

---
+++ SetModuleUnitTests/SetModelsTestsExtensions.swift
@@ -0,0 +1,34 @@
+//
+//  SetModelsTestsExtensions.swift
+//  SetModuleUnitTests
+//
+//  Created by Artem Shcherban on 28.04.2022.
+//
+
+import UIKit
+@testable import Collections
+
+extension SetModelsTests {
+
+    func inputString(in textField: Int, _ inputedString: String) -> String {
+        var expectedString = ""
+        inputedString.forEach { character in
+            expectedString += String(character)
+            expectedString = modelUnderTest.inputHandling(textField, expectedString)
+        }
+        return expectedString
+    }
+
+    func deleteString(in textField: Int, _ deletingString: String) {
+        var bufferString = deletingString
+        while bufferString.count != 0 {
+            bufferString.removeLast()
+            _ = modelUnderTest.inputHandling(textField, bufferString)
+        }
+    }
+
+    func createSets() {
+        _ = inputString(in: upperTextField, firstInputedString)
+        _ = inputString(in: bottomTextField, secondInputedString)
+    }
+}
