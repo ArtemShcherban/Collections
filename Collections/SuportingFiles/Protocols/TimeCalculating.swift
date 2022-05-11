5c6322fb026a36e2ab9dd9fe5b386bba22c635e0
Artem Shcherban <artshcherban@gmail.com>
2022-05-11 08:22:04 +0200

    TimeCalculating modified

--- Collections/SuportingFiles/Protocols/TimeCalculating.swift
+++ Collections/SuportingFiles/Protocols/TimeCalculating.swift
@@ -8,22 +8,13 @@
 import Foundation
 
 protocol TimeCalculating {
+    
     var startTime: DispatchTime { get }
     
     var endTime: DispatchTime { get }
-    
-    var timeInterval: String { get }
 }
 
 extension TimeCalculating {
-    
-    var startTime: DispatchTime {
-        return DispatchTime.init(uptimeNanoseconds: 0)
-    }
-    
-    var endTime: DispatchTime {
-        return DispatchTime.init(uptimeNanoseconds: 0)
-    }
     
     var timeInterval: String {
         let nanoTime = (endTime.uptimeNanoseconds - startTime.uptimeNanoseconds)
